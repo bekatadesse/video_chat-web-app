4e5f0ebc4c95b761c2be8e787b114f66689c5234
bekatadesse <bekatadesse5@gmail.com>
2023-05-21 22:49:46 +0300

    user registration and login

--- video_chat/video_call/views.py
+++ video_chat/video_call/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
 # Create your views here.
+
+def index(request):
+    return render(request, 'video_call/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        # check if form is submited
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            # check if username do not exist
+            # submited password is same and valid
+            user = form.save()
+
+            login(request, user)
+            # this method will take submited form and request ask login
+
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'video_call/signup.html', {'form': form})
